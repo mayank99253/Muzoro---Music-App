19a4bed778ce0bb355165bc4d56be72a07dfe60a
mayanksrivastava922 <catvideos999yt@gmail.com>
2026-05-20 20:10:15 +0530

    signup , login , logout implemented

--- backend/src/app.js
+++ backend/src/app.js
@@ -1,8 +1,11 @@
 //create the server
 import express from "express";
+import { authrouter } from "./routes/auth.route.js";
 
 const app = express();
 
 app.use(express.json());
+
+app.use("/api/auth" , authrouter);
 
 export { app };
