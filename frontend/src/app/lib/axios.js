70fe6149723d9390c8a13f834d138b4e1f1cf9cc
mayanksrivastava922 <catvideos999yt@gmail.com>
2026-08-04 22:08:09 +0530

    Delpoyment Done

--- frontend/src/app/lib/axios.js
+++ frontend/src/app/lib/axios.js
@@ -3,7 +3,7 @@ import axios from "axios"
 // "https://muzoro-music-app.onrender.com/api" for production
 
 export const AxiosInstance = axios.create({
-  baseURL: import.meta.env.VITE_API_DEPLOY_URL || import.meta.env.VITE_API_BASE_URL,
+  baseURL:import.meta.env.VITE_API_BASE_URL || '/api',
   withCredentials: true,
   // headers: {
   //   'Content-Type': 'multipart/form-data',
