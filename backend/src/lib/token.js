84c1b882477d6d38a57cf4e3129e4fce19b7374b
mayanksrivastava922 <catvideos999yt@gmail.com>
2026-08-10 21:59:38 +0530

    token in production

--- backend/src/lib/token.js
+++ backend/src/lib/token.js
@@ -23,7 +23,7 @@ export const generateToken = (res, userId, role) => {
     
     res.cookie("refreshToken", token, {
         httpOnly: true,
-        secure: ENV.NODE_ENV === "production",
+        secure: ENV.NODE_ENV === "production" ,
         sameSite: "lax",
         maxAge: 7 * 24 * 60 * 60 * 1000
     });
